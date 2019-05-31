1c5177a1531d134a3fc1f6120b8e2ee42dc451c9
rasool <rasool1993@gmail.com>
2019-05-31 05:34:13 -0700

    income the models and some beautifications

--- web/models.py
+++ web/models.py
@@ -1,10 +1,20 @@
 from __future__ import unicode_literals
+from django.utils.encoding import python_2_unicode_compatible
 from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
-
+@python_2_unicode_compatible
 class Expense(models.Model):
     text = models.CharField(max_length = 200)
     date = models.DateTimeField()
     amount = models.BigIntegerField()
     user = models.ForeignKey(User, on_delete = True)
+
+    def __str__(self):
+        return self.text
+
+class Income(models.Model):
+    text = models.CharField(max_length = 255)
+    date = models.DateTimeField()
+    amount = models.BigIntegerField()
+    user = models.ForeignKey(User,on_delete=True)
